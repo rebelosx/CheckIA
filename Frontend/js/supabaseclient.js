a63113a40a27e5923dc44582c0eec9a73cfef0af
joao-vss77 <victorsilvaesantos@gmail.com>
2026-09-25 19:59:58 -0300

    fix: correção de duplicata no cadastro.html e banco de dados de usuários funcionando.

--- Frontend/js/supabaseclient.js
+++ Frontend/js/supabaseclient.js
@@ -1,4 +1,6 @@
-const supabase = window.supabase.createClient(
-  'https://tbrasnqvnghpyqpaktul.supabase.co',
-  'sb_publishable_-b2MEl15msjCBl6hyUzY6w_MI8v2FEG'
-);
+// js/supabaseClient.js
+
+const SUPABASE_URL = 'https://tbrasnqvnghpyqpaktul.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRicmFzbnF2bmdocHlxcGFrdHVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyNzAzODYsImV4cCI6MjEwNTg0NjM4Nn0.bx-q6DLickm3Fsnl3qwjkmSoogukY_FHXtBfGFIheVk';
+
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
