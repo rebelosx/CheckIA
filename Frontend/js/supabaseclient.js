8cfa5315e50583567132c401d7d485a5d3f5b6af
joao-vss77 <victorsilvaesantos@gmail.com>
2026-09-24 21:21:59 -0300

    tentativas com o bt entrar

--- Frontend/js/supabaseclient.js
+++ Frontend/js/supabaseclient.js
@@ -1,4 +1,4 @@
 const supabase = window.supabase.createClient(
-  'https://tbrasnqvnghpyqpaktul.supabase.co/rest/v1/',
-  'sb_publishable_-b2MELl5msjCBl6hyUzY6w_MI8v2FEG'
+  'https://tbrasnqvnghpyqpaktul.supabase.co',
+  'sb_publishable_-b2MEl15msjCBl6hyUzY6w_MI8v2FEG'
 );
